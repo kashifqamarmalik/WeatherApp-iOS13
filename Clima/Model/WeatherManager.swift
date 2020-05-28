304cbc3b3e6a891ecaae1ed0efc39312313d2237
Kashif  Malik <kashifqamar111@gmail.com>
2020-05-28 18:08:31 +0300

    first commit

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by iosdev on 20.5.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by iosdev on 25.5.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
